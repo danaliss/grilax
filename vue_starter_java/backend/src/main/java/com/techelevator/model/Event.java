5d3ff25c14d563952e3343ee4fea2948387e7026
Jared Smithers <SmithersJJ1@live.com>
2019-12-09 17:07:21 -0500

    added the pojo for EVENT

---
+++ vue_starter_java/backend/src/main/java/com/techelevator/model/Event.java
@@ -0,0 +1,72 @@
+package com.techelevator.model;
+
+import java.time.LocalDateTime;
+
+
+
+public class Event{
+	
+	private String name;
+	private long id;
+	private LocalDateTime date;
+	private String time;
+	private String description;
+	private LocalDateTime deadline;
+	private long addressId;
+	private long menuId;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public LocalDateTime getDate() {
+		return date;
+	}
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public LocalDateTime getDeadline() {
+		return deadline;
+	}
+	public void setDeadline(LocalDateTime deadline) {
+		this.deadline = deadline;
+	}
+	public long getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(long addressId) {
+		this.addressId = addressId;
+	}
+	public long getMenuId() {
+		return menuId;
+	}
+	public void setMenuId(long menuId) {
+		this.menuId = menuId;
+	}
+	
+	
+	
+	
+	
+	
+}
