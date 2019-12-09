9bb316189389115a193ccd664c1c2668a01d55d5
Julia R Brooks <jrbrookshorn@gmail.com>
2019-12-09 14:46:52 -0500

    database name in spring updated. database name in create db script updated. login vue app styled

--- vue_starter_java/frontend/src/App.vue
+++ vue_starter_java/frontend/src/App.vue
@@ -6,3 +6,30 @@
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+body {
+  background: yellow;
+  background: radial-gradient(circle, yellow 50%, orange 100%);
+}
+#app {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 20px;
+}
+h1 {
+  grid-row: 1;
+  grid-column: 1 / 4;
+  text-align:center;
+  color:#ff009d;
+  text-transform: uppercase;
+  font-family: 'Roboto Condensed', sans-serif;
+}
+
+</style>
