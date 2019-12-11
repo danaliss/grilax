dfe7cfc3197b18646b868b807c9fd9f2ba324089
Jessica Berkowitz <Shecka826@gmail.com>
2019-12-11 13:48:46 -0500

    A few more changes to the database removed the menu table and changed the datatype of zip

--- vue_starter_java/backend/database/DummyData.sql
+++ vue_starter_java/backend/database/DummyData.sql
@@ -1,7 +1,6 @@
 SELECT * FROM users;
 SELECT * FROM address;
 SELECT * FROM event;
-SELECT * FROM menu;
 SELECT * FROM food;
 SELECT * FROM orders;
 SELECT * FROM event_attendees;
@@ -13,16 +12,14 @@ VALUES ('Shecka', 'cookout123', 'qwerty', 'Shecka826@gmail.com');
 INSERT INTO address (street_address, city, state, zip)
 VALUES ('123 Very Fake Street', 'Pittsburgh', 'PA', '15212');
 
-INSERT INTO event (event_name, event_date, event_time, description, deadline, menu_id, address_id)
-VALUES ('Christmas Cookout', '12-20-2019', '3pm', 'A fun get together for Christmas!', '12-10-2019', '1', '1');
+INSERT INTO event (event_name, event_date, event_time, description, deadline, address_id)
+VALUES ('Christmas Cookout', '12-20-2019', '3pm', 'A fun get together for Christmas!', '12-10-2019', '1');
 
-INSERT INTO menu (event_id) VALUES ('1');
-
-INSERT INTO food (food_name, vegetarian, vegan, gluten_free, nut_free, description, menu_id)
+INSERT INTO food (food_name, vegetarian, vegan, gluten_free, nut_free, description, event_id)
 VALUES ('Hamburger', 'false', 'false', 'false', 'true', 'Hamburger with all the fixings', '1');
-INSERT INTO food (food_name, vegetarian, vegan, gluten_free, nut_free, description, menu_id)
+INSERT INTO food (food_name, vegetarian, vegan, gluten_free, nut_free, description, event_id)
 VALUES ('Hotdog', 'false', 'false', 'false', 'true', 'Hotdog with all the fixings', '1');
-INSERT INTO food (food_name, vegetarian, vegan, gluten_free, nut_free, description, menu_id)
+INSERT INTO food (food_name, vegetarian, vegan, gluten_free, nut_free, description, event_id)
 VALUES ('Paneer Tikka Masala', 'true', 'false', 'true', 'true', 'Indian cheese in a spicy sauce', '1');
 
 INSERT INTO orders (event_id, user_id, food_id, status, quantity)
