26681c1464b291bcbf13c720d1a1b262ac3aa8d6
Dan Podobnik <daniel.podobnik@gmail.com>
2019-12-14 01:18:57 -0500

    A heck of a lot of data validation and JSON responses

--- vue_starter_java/backend/src/main/java/com/techelevator/controller/EventController.java
+++ vue_starter_java/backend/src/main/java/com/techelevator/controller/EventController.java
@@ -29,7 +29,6 @@ import com.techelevator.model.pojo.Event;
 import com.techelevator.model.pojo.EventAttendees;
 import com.techelevator.model.pojo.User;
 
-
 @RestController
 @CrossOrigin
 @RequestMapping("/api")
