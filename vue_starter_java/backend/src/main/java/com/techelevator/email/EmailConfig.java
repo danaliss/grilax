0509589e4f8efa5d20417307b2d61baabef36c24
Dan Podobnik <daniel.podobnik@gmail.com>
2019-12-15 19:36:47 -0500

    Added ability to send mail

---
+++ vue_starter_java/backend/src/main/java/com/techelevator/email/EmailConfig.java
@@ -0,0 +1,33 @@
+package com.techelevator.email;
+
+public class EmailConfig {
+	private String fromAddress;
+	private String host;
+	private String port;
+	private String password;
+	
+	public String getFromAddress() {
+		return fromAddress;
+	}
+	public void setFromAddress(String fromAddress) {
+		this.fromAddress = fromAddress;
+	}
+	public String getHost() {
+		return host;
+	}
+	public void setHost(String host) {
+		this.host = host;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPort() {
+		return port;
+	}
+	public void setPort(String port) {
+		this.port = port;
+	}
+}
