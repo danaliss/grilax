d6010dbf5985c0e40b755568363fb0980a3b5bc7
Dan Podobnik <daniel.podobnik@gmail.com>
2019-12-18 12:36:59 -0500

    Somehow my Rsvp.vue changes were overridden

--- vue_starter_java/frontend/src/views/Rsvp.vue
+++ vue_starter_java/frontend/src/views/Rsvp.vue
@@ -26,7 +26,7 @@
                 </label>
             </div>
 
-       
+            <div v-if="rsvp.attending==='true'">
                 <div class="form-group">
                     <h3>Your First Name</h3>
                     <label for="firstName">
@@ -46,24 +46,23 @@
                     </label>
                 </div>
 
-      <div v-if="rsvp.attending==='true'">
                 <div class = "form-group">
                     <h3>How many adult guests will you bring?</h3>
-                    <input type="number" v-model="rsvp.adultGuests" min="0" max="10" class="btn btn-secondary col-md-1" @blur="correctPeople()" />
+                    <input type="number" v-model="rsvp.adultGuests" min="0" max="10" class="btn btn-secondary col-md-1" @blur="correctAdultPeople()" />
                 </div>
                 <div class = "form-group">
                     <h3>How many children will you bring?</h3>
-                    <input type="number" v-model="rsvp.childGuests" min="0" max="10" class="btn btn-secondary col-md-1" @blur="correctPeople()" />
+                    <input type="number" v-model="rsvp.childGuests" min="0" max="10" class="btn btn-secondary col-md-1" @blur="correctChildPeople()" />
                 </div>
 
                 <div class="rsvp-menu" v-for="(personNumber,index) in totalNumberOfPeople" v-bind:key="index">
-                    <h2>Order for {{personLabel(index)}}</h2>
-                    <h3>Please pick one of the following entrees</h3>
+                    <h2 v-if="entree.length || side.length || dessert.length">Order for {{personLabel(index)}}</h2>
+                    <h3 v-if="entree.length">Please pick one of the following entrees</h3>
 
                     <div class = "food-btn container-fluid btn-group-vertical btn-group-toggle col-md-8 form-group" 
                                 v-for="food in entree" v-bind:key="food.foodId">
-                        <label class="btn btn-secondary btn-lg btn-block" type="radio" v-bind:class="{ active: rsvp.food[index] != undefined && rsvp.food[index].foodId===food.foodId }">
-                        <input name ="radio-food" type="radio" v-bind:value="food.foodId" v-model="rsvp.food[index].foodId" />
+                        <label class="btn btn-secondary btn-lg btn-block" type="radio" v-bind:class="{ active: rsvp.food[index] != undefined && rsvp.food[index].entreeId===food.foodId }">
+                        <input name ="radio-food" type="radio" v-bind:value="food.foodId" v-model="rsvp.food[index].entreeId" />
                             <h4>{{food.foodName}}</h4>
                             <h5 v-if="food.glutenFree">Gluten Free </h5>
                             <h5 v-if="food.nutFree">Nut Free </h5>
@@ -74,13 +73,37 @@
                     </div>
                     <!--rsvp-menu v-for="food in entree" v-bind:foodItem="food" v-bind:key="food.foodId"></rsvp-menu-->
                     
-                    <h3>Choose up to two sides</h3>
-                    <rsvp-menu v-for="food in side" v-bind:foodItem="food" v-bind:key="food.foodId"></rsvp-menu>
+                    <h3 v-if="side.length">Choose up to {{ maxSidesText }} side{{ isPlural(maxSides) }}</h3>
+                    <!--rsvp-menu v-for="food in side" v-bind:foodItem="food" v-bind:key="food.foodId"></rsvp-menu-->
+                    <div class = "food-btn container-fluid btn-group-vertical btn-group-toggle col-md-8 form-group" 
+                                v-for="food in side" v-bind:key="food.foodId">
+                        <label class="btn btn-secondary btn-lg btn-block" type="radio" v-bind:class="{ active: hasSide(index, food.foodId) }">
+                        <input name ="radio-food" type="checkbox" v-bind:value="food.foodId" @change="sideChanged(index, food.foodId)" />
+                            <h4>{{food.foodName}}</h4>
+                            <h5 v-if="food.glutenFree">Gluten Free </h5>
+                            <h5 v-if="food.nutFree">Nut Free </h5>
+                            <h5 v-if="food.vegetarian">Vegetarian </h5>  
+                            <h5 v-if="food.vegan">Vegan </h5>    
+                            <p>{{food.description}}</p>
+                        </label>
+                    </div>
 
-                    <h3>Choose one dessert</h3>
-                    <rsvp-menu v-for="food in dessert" v-bind:foodItem="food" v-bind:key="food.foodId"></rsvp-menu>
+                    <h3 v-if="dessert.length">Choose one dessert</h3>
+                    <!--rsvp-menu v-for="food in dessert" v-bind:foodItem="food" v-bind:key="food.foodId"></rsvp-menu-->
+                    <div class = "food-btn container-fluid btn-group-vertical btn-group-toggle col-md-8 form-group" 
+                                v-for="food in dessert" v-bind:key="food.foodId">
+                        <label class="btn btn-secondary btn-lg btn-block" type="radio" v-bind:class="{ active: rsvp.food[index] != undefined && rsvp.food[index].dessertId===food.foodId }">
+                        <input name ="radio-food" type="radio" v-bind:value="food.foodId" v-model="rsvp.food[index].dessertId" />
+                            <h4>{{food.foodName}}</h4>
+                            <h5 v-if="food.glutenFree">Gluten Free </h5>
+                            <h5 v-if="food.nutFree">Nut Free </h5>
+                            <h5 v-if="food.vegetarian">Vegetarian </h5>  
+                            <h5 v-if="food.vegan">Vegan </h5>    
+                            <p>{{food.description}}</p>
+                        </label>
+                    </div>
 
-                    <h3>Beverage options at the party</h3>
+                    <h3 v-if="beverage.length">Beverage options at the party</h3>
                     <ul>
                         <li v-for="drink in beverage" v-bind:key="drink.foodId">{{drink.foodName}} {{drink.description}}</li>
                     </ul>
@@ -107,6 +130,8 @@ export default {
     },
     data(){
         return {
+            maxSides: 2,
+            maxSidesText: "two",
             rsvp: {
               eventId:'',
               attending: '',
@@ -115,7 +140,7 @@ export default {
               adultGuests: 0,
               childGuests: 0,
               food: [
-                  { foodId: null }
+                  { entreeId: null, sideIds: [], dessertId: null }
               ]
             },
             menu: [],
@@ -159,24 +184,25 @@ export default {
             if( this.rsvp.attending === '' ) {
                 this.errors.push("Please say if you're attending");
             } else if( this.rsvp.attending === 'true' ) {
-                this.rsvp.food.forEach((current,index)=>{
-                    if( current.foodId == undefined ) {
-                        this.errors.push("Please complete the food order for "+this.personLabel(index));
-                    }
-                })
-            } else {
-                this.adultGuests = 0;
-                this.childGuests= 0;
-
-            }
-
-            // check more form
+                // check more form
                 if( this.rsvp.firstName.trim() === '' ) {
                     this.errors.push("Please tell us your first name");
                 }
                 if( this.rsvp.lastName.trim() === '' ) {
                     this.errors.push("Please tell us your last name");
                 }
+                this.rsvp.food.forEach((current,index)=>{
+                    if( this.entree.length && current.entreeId == undefined ) {
+                        this.errors.push("Please complete the entree order for "+this.personLabel(index));
+                    }
+                    if( current.sideIds.length < this.side.length ) {
+                        this.errors.push("Please complete the side order for "+this.personLabel(index));
+                    }
+                    if( this.dessert.length && current.dessertId == undefined ) {
+                        this.errors.push("Please complete the dessert order for "+this.personLabel(index));
+                    }
+                })
+            }
 
             if( this.errors.length === 0 ) {
                 let path = this.rsvp.attending === 'true' ? "accept" : "decline";
@@ -199,16 +225,31 @@ export default {
                     //// pack it for quantity
                     let foodSend = this.rsvp.food.reduce((acc, cur)=>{
                         // see if food is currently there
-                        let found = false;
                         for( let food of acc ) {
-                            if( food.foodId === cur.foodId ) {
+                            if( food.foodId === cur.entreeId ) {
                                 food.quantity++;
-                                found = true;
-                                break;
+                                cur.entreeId = null;
+                            }
+                            if( food.foodId === cur.dessertId ) {
+                                food.quantity++;
+                                cur.dessertId = null;
+                            }
+                            for( let sideIdx in cur.sideIds ) {
+                                if( food.foodId === cur.sideIds[sideIdx] ) {
+                                    food.quantity++;
+                                    cur.sideIds[sideIdx] = null;
+                                }
                             }
                         }
-                        if( !found ) {
-                            acc.push({ foodId: cur.foodId, quantity: 1 });
+                        // check for nulls
+                        if( cur.entreeId != null ) {
+                            acc.push({ foodId: cur.entreeId, quantity: 1 });
+                        }
+                        if( cur.dessertId != null ) {
+                            acc.push({ foodId: cur.dessertId, quantity: 1 });
+                        }
+                        for( let side of cur.sideIds ) {
+                            acc.push({ foodId: side, quantity: 1 });
                         }
 
                         return acc;
@@ -251,7 +292,7 @@ export default {
                 if( newVal < oldVal ) {
                     this.rsvp.food.splice(oldVal-i, 1);
                 } else {
-                    this.rsvp.food.splice(1+oldVal, 0, { foodId: null });
+                    this.rsvp.food.splice(1+oldVal, 0, { entreeId: null, sideIds: [], dessertId: null });
                 }
             }
         },
@@ -260,22 +301,53 @@ export default {
                 if( newVal < oldVal ) {
                     this.rsvp.food.pop();
                 } else {
-                    this.rsvp.food.push({ foodId: null });
+                    this.rsvp.food.push({ entreeId: null, sideIds: [], dessertId: null });
                 }
             }
         },
-        correctPeople: function() {
-            let oldVal = this.rsvp.food.length-1-this.rsvp.childGuests;
-            this.adultGuestsChange(0, oldVal);
+        correctAdultPeople: function() {
             if( this.rsvp.adultGuests === '' ) {
-                this.rsvp.adultGuests = 0;
+                this.rsvp.adultGuests = '0';
+                let childCount = parseInt(this.rsvp.childGuests);
+                // check the count
+                let oldVal = this.rsvp.food.length - childCount - 1;
+                if( oldVal > 0 ) {
+                    this.adultGuestsChange(0, oldVal);
+                }
             }
-            
-            oldVal = this.rsvp.food.length-1-this.rsvp.adultGuests;
-            this.childGuestsChange(0, oldVal);
+        },
+        correctChildPeople: function() {
             if( this.rsvp.childGuests === '' ) {
-                this.rsvp.childGuests = 0;
+                this.rsvp.childGuests = '0';
+                let adultCount = parseInt(this.rsvp.adultGuests);
+                // check the count
+                let oldVal = this.rsvp.food.length - adultCount - 1;
+                if( oldVal > 0 ) {
+                    this.childGuestsChange(0, oldVal);
+                }
             }
+        },
+        hasSide: function(index, checkId) {
+            for( let side of this.food.sideIds ) {
+                if( side == checkId ) {
+                    return true;
+                }
+            }
+            return false;
+        },
+        sideChanged: function(index, changedId) {
+            // see if changedId is present
+            let exists = this.rsvp.food[index].sideIds.findIndex((element)=>element==changedId);
+            if( exists === -1 ) {
+                if( this.rsvp.food[index].sideIds.unshift(changedId) > this.maxSides ) {
+                    this.rsvp.food[index].sideIds.pop();
+                }
+            } else {
+                this.rsvp.food[index].sideIds.splice(exists, 1);
+            }
+        },
+        isPlural(num) {
+            return num==1?"":"s";
         }
     },
     watch: {
