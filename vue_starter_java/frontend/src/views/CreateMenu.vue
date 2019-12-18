47412a059d6b0e65b0829f96d610f8fb60dcba10
Dan Podobnik <daniel.podobnik@gmail.com>
2019-12-18 13:23:50 -0500

    Allowed for unlimited food choices

--- vue_starter_java/frontend/src/views/CreateMenu.vue
+++ vue_starter_java/frontend/src/views/CreateMenu.vue
@@ -4,30 +4,26 @@
         <br><br>
         
         <h4>Entrees</h4>
-        <entree-input v-model="entree1" />
-        <entree-input v-model="entree2" />
-        <entree-input v-model="entree3" />
-
+        <entree-input v-for="(entree,index) in entrees" :key="index+'-entree'" v-model="entrees[index]" />
+        <button @click.prevent="addEntree()">Add Another Entree</button>
         
         <br><br>
         <h4>Sides</h4>
-        <side-input v-model="side1"/>
-        <side-input v-model="side2"/>
-        <side-input v-model="side3"/>
-
+        <side-input v-for="(side,index) in sides" :key="index+'-side'" v-model="sides[index]"/>
+        <button @click.prevent="addSide()">Add Another Side</button>
 
         <br><br>
 
         <h4>Beverages</h4>
-        <beverage-input v-model="beverage1"/>
-        <beverage-input v-model="beverage2"/>
-        <beverage-input v-model="beverage3"/>
+        <beverage-input v-for="(beverage,index) in beverages" :key="index+'-beverage'" v-model="beverages[index]"/>
+        <button @click.prevent="addBeverage()">Add Another Beverage</button>
         
         <br><br>
         <h4>Desserts</h4>
-        <dessert-input v-model="dessert1"/>
-        <dessert-input v-model="dessert2"/>
-        <dessert-input v-model="dessert3"/>
+        <dessert-input v-for="(dessert,index) in desserts" :key="index+'-dessert'" v-model="desserts[index]"/>
+        <button @click.prevent="addDessert()">Add Another Dessert</button>
+
+        <br /><br />
 
         <button type="submit" class = "btn btn-secondary">Create Menu</button>
         
@@ -35,11 +31,6 @@
  
   
     </form>
-
-
-
-
-
 </div>
     
 </template>
@@ -59,167 +50,141 @@ export default {
 },
     data() {
         return {
-            
-            entree1 : {
-                foodName : '',
-                vegetarian : false,
-                vegan : false,
-                glutenFree : false,
-                nutFree : false,
-                description : '',
-                foodCategory : 'Entree'
-            },
-            entree2 : {
-                foodName : '',
-                vegetarian : false,
-                vegan : false,
-                glutenFree : false,
-                nutFree : false,
-                description : '',
-                foodCategory : 'Entree'
-            },
-            entree3 : {
-                foodName : '',
-                vegetarian : false,
-                vegan : false,
-                glutenFree : false,
-                nutFree : false,
-                description : '',
-                foodCategory : 'Entree'
-            },
-            side1 : {
-                foodName : '',
-                vegetarian : false,
-                vegan : false,
-                glutenFree : false,
-                nutFree : false,
-                description : '',
-                foodCategory : 'Side'
-            },
-            side2 : {
-                foodName : '',
-                vegetarian : false,
-                vegan : false,
-                glutenFree : false,
-                nutFree : false,
-                description : '',
-                foodCategory : 'Side'
-            },
-            side3 : {
-                foodName : '',
-                vegetarian : false,
-                vegan : false,
-                glutenFree : false,
-                nutFree : false,
-                description : '',
-                foodCategory : 'Side'
-            },
-            dessert1 : {
-                foodName : '',
-                vegetarian : false,
-                vegan : false,
-                glutenFree : false,
-                nutFree : false,
-                description : '',
-                foodCategory : 'Dessert'
-            },
-            dessert2 : {
-                foodName : '',
-                vegetarian : false,
-                vegan : false,
-                glutenFree : false,
-                nutFree : false,
-                description : '',
-                foodCategory : 'Dessert'
-            },
-            dessert3 : {
-                foodName : '',
-                vegetarian : false,
-                vegan : false,
-                glutenFree : false,
-                nutFree : false,
-                description : '',
-                foodCategory : 'Dessert'
-            },
-            beverage1 : {
-                foodName : '',
-                vegetarian : false,
-                vegan : false,
-                glutenFree : false,
-                nutFree : false,
-                description : '',
-                foodCategory : 'Beverage'
-            },
-            beverage2 : {
-                foodName : '',
-                vegetarian : false,
-                vegan : false,
-                glutenFree : false,
-                nutFree : false,
-                description : '',
-                foodCategory : 'Beverage'
-            },
-            beverage3 : {
-                foodName : '',
-                vegetarian : false,
-                vegan : false,
-                glutenFree : false,
-                nutFree : false,
-                description : '',
-                foodCategory : 'Beverage'
-            },
+            entrees: [{
+                foodName: '',
+                vegetarian: false,
+                vegan: false,
+                glutenFree: false,
+                nutFree: false,
+                description: '',
+                foodCategory: 'Entree'
+            }],
+            sides: [{
+                foodName: '',
+                vegetarian: '',
+                vegan: false,
+                glutenFree: false,
+                nutFree: false,
+                description: '',
+                foodCategory: 'Side'
+            }],
+            desserts: [{
+                foodName: '',
+                vegetarian: false,
+                vegan: false,
+                glutenFree: false,
+                nutFree: false,
+                description: '',
+                foodCategory: 'Dessert'
+            }],
+            beverages: [{
+                foodName: '',
+                vegetarian: false,
+                vegan: false,
+                glutenFree: false,
+                nutFree: false,
+                description: '',
+                foodCategory: 'Beverage'
+            }],
         }
     },
     methods: {
-                    fetcherMethod(menuItem){
-                        if(menuItem.foodName !== ''){
-                        fetch(`${process.env.VUE_APP_REMOTE_API}/api/event/${this.$route.params.eventId}/menu`, {
-                        method: 'POST',
-                        headers: {
-                          "Authorization": "Bearer "+ auth.getToken(),
-                          "Accept": 'application/json',
-                          'Content-Type': 'application/json',
-                
-                        },
-                        body: JSON.stringify(menuItem),
-                      })
-                    }
+        fetcherMethod(menuItem){
+            if(menuItem.foodName !== ''){
+                return fetch(`${process.env.VUE_APP_REMOTE_API}/api/event/${this.$route.params.eventId}/menu`, {
+                    method: 'POST',
+                    headers: {
+                        "Authorization": "Bearer "+ auth.getToken(),
+                        "Accept": 'application/json',
+                        'Content-Type': 'application/json',
+            
                     },
-    createMenu() {
-      fetch(`${process.env.VUE_APP_REMOTE_API}/api/event/${this.$route.params.eventId}/menu`, {
-        method: 'POST',
-        headers: {
-          "Authorization": "Bearer "+ auth.getToken(),
-          "Accept": 'application/json',
-          'Content-Type': 'application/json',
-
+                    body: JSON.stringify(menuItem),
+                })
+            }
+            return null;
         },
-        body: JSON.stringify(this.entree1),
-      })
-      .then(this.fetcherMethod(this.entree2))
-      .then(this.fetcherMethod(this.entree3))
-      .then(this.fetcherMethod(this.side1))
-      .then(this.fetcherMethod(this.side2))
-      .then(this.fetcherMethod(this.side3))
-      .then(this.fetcherMethod(this.beverage1))
-      .then(this.fetcherMethod(this.beverage2))
-      .then(this.fetcherMethod(this.beverage3))
-      .then(this.fetcherMethod(this.dessert1))
-      .then(this.fetcherMethod(this.dessert2))
-      .then(this.fetcherMethod(this.dessert3))
+        addEntree() {
+            this.entrees.push({
+                foodName: '',
+                vegetarian: false,
+                vegan: false,
+                glutenFree: false,
+                nutFree: false,
+                description: '',
+                foodCategory: 'Entree'
+            });
+        },
+        addBeverage() {
+            this.beverages.push({
+                foodName: '',
+                vegetarian: false,
+                vegan: false,
+                glutenFree: false,
+                nutFree: false,
+                description: '',
+                foodCategory: 'Beverage'
+            });
+        },
+        addDessert() {
+            this.desserts.push({
+                foodName: '',
+                vegetarian: false,
+                vegan: false,
+                glutenFree: false,
+                nutFree: false,
+                description: '',
+                foodCategory: 'Dessert'
+            });
+        },
+        addSide() {
+            this.sides.push({
+                foodName: '',
+                vegetarian: '',
+                vegan: false,
+                glutenFree: false,
+                nutFree: false,
+                description: '',
+                foodCategory: 'Side'
+            });
+        },
+        createMenu() {
+            let promises = [];
 
-        
-        .then((response) => {
-          if (response.ok) {
-            this.$router.push({ path: `/${this.$route.params.eventId}/eventdetails`, query: { createEventStatus: 'success' } });
-          } else {
-            this.registrationErrors = true;
-          }
-        })
+            for( let entree of this.entrees ) {
+                let promise = this.fetcherMethod(entree);
+                if( promise != null ) {
+                    promises.push(promise);
+                }
+            }
+            for( let side of this.sides ) {
+                let promise = this.fetcherMethod(side);
+                if( promise != null ) {
+                    promises.push(promise);
+                }
+            }
+            for( let dessert of this.desserts ) {
+                let promise = this.fetcherMethod(dessert);
+                if( promise != null ) {
+                    promises.push(promise);
+                }
+            }
+            for( let beverage of this.beverages ) {
+                let promise = this.fetcherMethod(beverage);
+                if( promise != null ) {
+                    promises.push(promise);
+                }
+            }
 
-        .then((err) => console.error(err));
+            Promise.all(promises).then(response=>{
+                if (response.ok) {
+                    this.$router.push({ path: `/${this.$route.params.eventId}/eventdetails`, query: { createEventStatus: 'success' } });
+                } else {
+                    this.registrationErrors = true;
+                }
+            });
+        },
     },
-  },
 }
 </script>
 <style scoped>
