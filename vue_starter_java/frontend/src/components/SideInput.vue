63508fdbce3059b8ed87024b105da11fe5e37811
Dan Podobnik <daniel.podobnik@gmail.com>
2019-12-19 14:28:35 -0500

    Fixed create menu labels for checkboxes selecting the wrong checkboxes

--- vue_starter_java/frontend/src/components/SideInput.vue
+++ vue_starter_java/frontend/src/components/SideInput.vue
@@ -2,28 +2,28 @@
     <div class = "side-div">
         <div class="form-group">
             <label for="side-name">Side Name</label>
-            <input v-model="value.foodName" type="text" id="side-name" placeholder = "Salacious sound for your side" class= "form-control">
+            <input v-model="value.foodName" type="text" :id="getId('side-name')" placeholder = "Salacious sound for your side" class= "form-control">
         </div>
         <div class = "form-group">
             <label for="side-description">Side Description</label>
-            <textarea v-model="value.description" type="text-area" class="form-control" id="side-description" placeholder="Specifics sketching out your sustenance "/>
+            <textarea v-model="value.description" type="text-area" class="form-control" :id="getId('side-description')" placeholder="Specifics sketching out your sustenance "/>
         </div>
 
         <div class="form-check form-check-inline">
-                <input v-model="value.vegetarian"  class="form-check-input" type="checkbox" id="checkbox-vegetarian" value="true">
-                <label class="form-check-label" for="checkbox-vegetarian">Vegetarian</label>
+                <input v-model="value.vegetarian"  class="form-check-input" type="checkbox" :id="getId('checkbox-vegetarian')" value="true">
+                <label class="form-check-label" :for="getId('checkbox-vegetarian')">Vegetarian</label>
         </div>
         <div class="form-check form-check-inline">
-                <input v-model="value.vegan" class="form-check-input" type="checkbox" id="checkbox-vegan" value="true">
-                <label class="form-check-label" for="checkbox-vegan">Vegan</label>
+                <input v-model="value.vegan" class="form-check-input" type="checkbox" :id="getId('checkbox-vegan')" value="true">
+                <label class="form-check-label" :for="getId('checkbox-vegan')">Vegan</label>
         </div>
         <div class="form-check form-check-inline">
-            <input v-model="value.glutenFree" class="form-check-input" type="checkbox" id="checkbox-gluten-free" value="true">
-            <label class="form-check-label" for="checkbox-gluten-free">Gluten-Free</label>
+            <input v-model="value.glutenFree" class="form-check-input" type="checkbox" :id="getId('checkbox-gluten-free')" value="true">
+            <label class="form-check-label" :for="getId('checkbox-gluten-free')">Gluten-Free</label>
         </div>
         <div class="form-check form-check-inline">
-            <input v-model="value.nutFree" class="form-check-input" type="checkbox" id="checkbox-nut-free" value="true">
-            <label class="form-check-label" for="checkbox-nut-free">Nut-Free</label>
+            <input v-model="value.nutFree" class="form-check-input" type="checkbox" :id="getId('checkbox-nut-free')" value="true">
+            <label class="form-check-label" :for="getId('checkbox-nut-free')">Nut-Free</label>
         </div>
         <br><br><br>
     </div>
@@ -32,16 +32,24 @@
 export default {
     name: 'side-input',
     props: {
-            value: {
-                type: Object,
-                required: true
-            }
+        value: {
+            type: Object,
+            required: true
         },
-        watch: {
-            value() {
-                this.$emit('input', this.value);
-            }
+        index: {
+            type: Number
         }
+    },
+    methods: {
+        getId(type) {
+            return `side-${type}-${this.index}`;
+        }
+    },
+    watch: {
+        value() {
+            this.$emit('input', this.value);
+        }
+    }
 }
 </script>
 <style scoped>
